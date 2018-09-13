064ed436c6aa07f34e720ff83c116a38a4a4da20
putriaulianti <42660888+putriaulianti@users.noreply.github.com>
2018-09-13 08:55:35 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,36 @@
 <?php
+$baris=5;
+printPiramida(0);
+	function printPiramida($i) {
+	  global $baris;
+	  printJarak(0, $i);
+	  printPola($baris, $i);
+	  echo "<br>";
+	  if ($baris > $i++) printPiramida($i);
+	}
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+	function printJarak ($j,$i) {
+		global $baris;
+		if($j>$baris-$i-1) {
+			echo "&nbsp;";
+			printJarak($j+1,$i);
+		}
+	}
+	function printPola ($a, $i) {
+		global $baris;
+		if($a>$baris-$i-1) {
+			echo "*&nbsp;";
+			printPola($a-1, $i);
+		}
+	}
+?>
+//========================================================================================================
+<?php
+	for ($i=1; $i<=5; $i++) { 
+		for ($p=$i; $p<=5; $p++) {
+			echo "$p"; 
+		}
+		echo "<br>";
+	}
+
 ?>
